131ac0428cffffed0c802234c980df90df23f1aa
Dmytro Shcherban <dimasahah15@gmail.com>
2025-12-03 18:02:51 +0200

    update: add tfstate

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,12 @@
 terraform {
   required_version = ">=0.13.0"
+
+  backend "s3" {
+    bucket = "terraform-state-pawnshop-shcherban"  
+    key    = "prod/terraform.tfstate"              
+    region = "eu-central-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
